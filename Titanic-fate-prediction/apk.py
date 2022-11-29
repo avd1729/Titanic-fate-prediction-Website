7e624f86c1f0c71b12dc874aabb8eadf484b85c8
Aravind M S <94891044+avd1729@users.noreply.github.com>
2022-11-29 22:34:29 +0530

    Add files via upload

---
+++ Titanic-fate-prediction/apk.py
@@ -0,0 +1,32 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+#Initialize the flask App
+app = Flask(__name__,static_folder='static')
+model = pickle.load(open('model.pkl', 'rb'))
+
+#default page of our web-app
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+#To use the predict button in our web-app
+@app.route('/predict1',methods=['POST'])
+def predict1():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    if prediction == 1:
+        prediction = 'Congratulations , you have survived the shipwreck :)'
+    else:
+        prediction = 'Sorry , you are no more !'
+        
+    return render_template('index.html', prediction_text=prediction)
+
+if __name__ == "__main__":
+    app.run(debug=True)
