b59490f071f2c9b4f9aa20d8ef6a29d937095604
avd <aravind19112003@gmail.com>
2023-11-10 14:19:05 +0530

    model folder change

--- app.py
+++ app.py
@@ -2,17 +2,21 @@ import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 
-#Initialize the flask App
-app = Flask(__name__,static_folder='static')
-model = pickle.load(open('model.pkl', 'rb'))
+# Initialize the flask App
+app = Flask(__name__, static_folder='static')
+model = pickle.load(open('models\model.pkl', 'rb'))
 
-#default page of our web-app
+# default page of our web-app
+
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
-#To use the predict button in our web-app
-@app.route('/predict1',methods=['POST'])
+# To use the predict button in our web-app
+
+
+@app.route('/predict1', methods=['POST'])
 def predict1():
     '''
     For rendering results on HTML GUI
@@ -25,8 +29,9 @@ def predict1():
         prediction = 'Congratulations , you have survived the shipwreck :)'
     else:
         prediction = 'Sorry , you are no more !'
-        
+
     return render_template('index.html', prediction_text=prediction)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
